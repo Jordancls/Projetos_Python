0db9e4f39d72227c38c7377543c484717befa1ab
Jordancls <jordanclsjp96@gmail.com>
2022-11-22 09:41:58 -0300

    Atualização_print

--- Gerador_de_Nome_de_Banda.py
+++ Gerador_de_Nome_de_Banda.py
@@ -9,4 +9,4 @@ Cidade = input("Qual é o nome da cidade em que você cresceu?\n")
 pet = input("Qual é o nome do teu animal de estimação?\n")
 
 #4. combine o nome de sua cidade e animal de estimação e mostre a eles o nome de sua banda
-print("O nome da sua banda pode ser " + street + " " + pet)
+print("O nome da sua banda pode ser " + Cidade + " " + pet)
